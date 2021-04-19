72135ba63e2b34974e4a0222062451577c3d0daf
james <035156@swcsd.us>
2021-04-19 11:29:47 -0400

    first start for pizza project

--- clicker/shopViewController.swift
+++ clicker/shopViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class shopViewController: UIViewController {
+class shopViewController: UIViewController{//,UITableViewDelegate,UITableViewDataSource {
+   
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +21,12 @@ class shopViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//       }
+//
+//       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//       }
 
 }
