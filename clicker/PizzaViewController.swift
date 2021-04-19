9f65e05b5731f0bc3c4d24b999166fac4bc9a695
james <035156@swcsd.us>
2021-04-19 11:21:21 -0400

    first form of project

---
+++ clicker/PizzaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PizzaViewController.swift
+//  clicker
+//
+//  Created by James Caldwell on 4/19/21.
+//  Copyright © 2021 James Caldwell. All rights reserved.
+//
+
+import UIKit
+
+class PizzaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
