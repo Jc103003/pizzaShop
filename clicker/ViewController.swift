69c78f5f71f77220885d41238961969de6e95bdf
james <035156@swcsd.us>
2021-04-19 10:57:39 -0400

    Initial Commit

---
+++ clicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clicker
+//
+//  Created by James Caldwell on 4/19/21.
+//  Copyright © 2021 James Caldwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
